52c142c0ddb9de7704990b3a6656273f3cfeb30b
Heli Wang <heli.h101@gmail.com>
2012-04-26 19:35:59 -0400

    adds read structure for running code

--- imperatum.py
+++ imperatum.py
@@ -1,1 +1,23 @@
-def read:
+values = []
+pc = 0
+
+def read(table, n, value):
+    if (len(table) < n):
+        table[n]=value
+    else:
+        table += [value]
+    n += 1
+    return
+
+read(values,pc,"potato")
+read(values,pc," is")
+read(values,pc,"KAWAII!!!")
+print values
+
+def hello(this):
+    print "hello, world!\n"
+    return this
+def main():
+    print hello("Again!")
+if __name__ == '__main__':
+    main()
